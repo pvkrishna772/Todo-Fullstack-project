7f8cf6e9d84d00796f23ae4d383f5fb732fa23b2
vamsikrishna pathipati <kv414861@gmail.com>
2026-03-10 23:57:20 -0500

    fix cors origin error

--- TodoBackend/server.js
+++ TodoBackend/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({origin:"https://mytodos-chi.vercel.app/",credentials:true}));
+app.use(cors({origin:"https://mytodos-chi.vercel.app",credentials:true}));
 app.use(express.json());
 app.use(cookieParser());
 

a3d239a8f356e3f3a2346a60b68267ac6268e460
vamsikrishna pathipati <kv414861@gmail.com>
2026-03-10 23:46:59 -0500

    update frontend API base URL to Render backend

--- reactproject/src/api/api.js
+++ reactproject/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL :"http://localhost:5000/api",
+    baseURL :" https://todo-fullstack-project-pm8d.onrender.com/api",
     withCredentials: true
 });
 
